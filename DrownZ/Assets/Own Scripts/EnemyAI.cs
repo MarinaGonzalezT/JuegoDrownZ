bf465da64a20a05e770c89bde72a2b90e5f1c68c
MarinaGonzalezT <marinagonzaleztorres98@gmail.com>
2025-06-11 15:11:27 +0200

    Arreglado el Surface y Perseguir del enemigo

--- DrownZ/Assets/Own Scripts/EnemyAI.cs
+++ DrownZ/Assets/Own Scripts/EnemyAI.cs
@@ -11,10 +11,12 @@ public class EnemyAI : MonoBehaviour
     public Transform player;
     public float attackRange = 2.5f;
     public float chaseRange = 10f;
+    public float loseRange = 20f;
     public float waitTime = 2f;
 
     private bool isWaiting = false;
     private float waitTimer = 0f;
+    private bool isChasing = false;
 
     private Animator animator;
     private bool isDead = false;
@@ -42,62 +44,78 @@ public class EnemyAI : MonoBehaviour
         float distance = Vector3.Distance(transform.position, player.position);
         animator.SetFloat("distanceToPlayer", distance);
 
-        if (distance > chaseRange)
+        if (isChasing && distance > loseRange)
         {
-            // Patrullar y pararse
-            animator.SetBool("seePlayer", false);
-            animator.SetBool("isAttacking", false);
+            agent.SetDestination(patrolPoints[currentPatrolIndex].position);
+            agent.speed = 2.2f;
 
-            if (isWaiting)
+            isChasing = false;
+        } 
+        else
+        {
+            if (distance > chaseRange)
             {
-                agent.isStopped = true;
-                animator.SetBool("isWalking", false);
-                waitTimer += Time.deltaTime;
+                PatrolBehavior();
+            }
+            else if (distance > attackRange)
+            {
+                isChasing = true;
 
-                if(waitTimer >= waitTime)
-                {
-                    isWaiting = false;
-                    waitTimer = 0f;
-                    currentPatrolIndex = (currentPatrolIndex + 1) % patrolPoints.Length;
-                    agent.SetDestination(patrolPoints[currentPatrolIndex].position);
-                    agent.isStopped = false;
-                    animator.SetBool("isWalking", true);
-                }
+                // Correr hacia el jugador
+                animator.SetBool("seePlayer", true);
+                animator.SetBool("isWalking", false);
+                animator.SetBool("isAttacking", false);
+
+                agent.isStopped = false;
+                agent.speed = 4.2f;
+                agent.SetDestination(player.position);
             }
             else
             {
-                agent.isStopped = false;
-                animator.SetBool("isWalking", true);
-
-                if (!agent.pathPending && agent.remainingDistance < 0.5f)
-                {
-                    isWaiting = true;
-                    waitTimer = 0f;
-                    agent.isStopped = true;
-                    animator.SetBool("isWalking", false);
-                }
+                // Atacar
+                animator.SetBool("isAttacking", true);
+                animator.SetTrigger("attackAlt");
+                agent.isStopped = true;
             }
-        }
-        else if (distance > attackRange)
-        {
-            // Correr hacia el jugador
-            animator.SetBool("seePlayer", true);
-            animator.SetBool("isWalking", false);
-            animator.SetBool("isAttacking", false);
-
-            agent.isStopped = false;
-            agent.speed = 4.2f;
-            agent.SetDestination(player.position);
-        }
-        else
-        {
-            // Atacar
-            animator.SetBool("isAttacking", true);
-            animator.SetTrigger("attackAlt");
-            agent.isStopped = true;
         }
     }
 
+    private void PatrolBehavior()
+    {
+        // Patrullar y pararse
+        animator.SetBool("seePlayer", false);
+        animator.SetBool("isAttacking", false);
+
+        if (isWaiting)
+        {
+            agent.isStopped = true;
+            animator.SetBool("isWalking", false);
+            waitTimer += Time.deltaTime;
+
+            if (waitTimer >= waitTime)
+            {
+                isWaiting = false;
+                waitTimer = 0f;
+                currentPatrolIndex = (currentPatrolIndex + 1) % patrolPoints.Length;
+                agent.SetDestination(patrolPoints[currentPatrolIndex].position);
+                agent.isStopped = false;
+                animator.SetBool("isWalking", true);
+            }
+        }
+        else
+        {
+            agent.isStopped = false;
+            animator.SetBool("isWalking", true);
+
+            if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            {
+                isWaiting = true;
+                waitTimer = 0f;
+                agent.isStopped = true;
+                animator.SetBool("isWalking", false);
+            }
+        }
+    }
     public void Die()
     {
         isDead = true;
