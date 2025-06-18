31d15276464fc16edda2c9360f2b327b1b6503ea
MarinaGonzalezT <marinagonzaleztorres98@gmail.com>
2025-06-19 00:08:16 +0200

    Carta implementada con transición a créditos
    Falta por implementar la escena de créditos real.

--- DrownZ/Assets/Own Scripts/Letter.cs
+++ DrownZ/Assets/Own Scripts/Letter.cs
@@ -1,16 +1,73 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Carta : MonoBehaviour
+namespace cowsins
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class Letter : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private CanvasGroup playerUIGroup;
+        [SerializeField] private Crosshair crosshair;
+        public GameObject letter;
+        private PlayerStats playerStats;
+        public float letterDuration = 8f;
+        public string creditsSceneName = "EscenaCreditos";
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Start()
+        {
+            if (letter != null)
+                letter.SetActive(false);
+        }
+
+        public void ShowLetter()
+        {
+            StartCoroutine(ShowLetterRoutine());
+        }
+
+        private IEnumerator ShowLetterRoutine()
+        {
+            letter.SetActive(true);
+
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            // Buscar PlayerStats
+            playerStats = FindFirstObjectByType<PlayerStats>();
+            if (playerStats != null)
+            {
+                playerStats.LoseControl();
+            }
+
+            playerUIGroup.alpha = 0;
+            playerUIGroup.interactable = false;
+            playerUIGroup.blocksRaycasts = false;
+
+            if (crosshair != null)
+                crosshair.SetVisibility(false);
+
+            yield return null;
+        }
+
+        public void GoToCredits()
+        {
+            letter.SetActive(false);
+
+            playerUIGroup.alpha = 1;
+            playerUIGroup.interactable = true;
+            playerUIGroup.blocksRaycasts = true;
+
+            if (crosshair != null)
+                crosshair.SetVisibility(true);
+
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            // Buscar PlayerStats
+            playerStats = FindFirstObjectByType<PlayerStats>();
+            if (playerStats != null)
+            {
+                playerStats.CheckIfCanGrantControl();
+            }
+
+            SceneManager.LoadScene(creditsSceneName);
+        }
     }
 }
